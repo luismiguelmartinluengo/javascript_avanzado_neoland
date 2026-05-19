a2083fd9bf5544f9b8da4690bb42e75a808b5953
luismiguelmartinluengo <luismiguelm@gmail.com>
2026-05-19 19:51:31 +0200

    formulario lista de la compra 20260519

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const txtNuevoArticulo = document.getElementById('txtNuevoArticulo')
 const btnAñadirArticulo = document.getElementById('btnAñadirArticulo')
 const ulLista = document.getElementById('ulLista')
+const articulosEnLista = []
 
 txtNuevoArticulo.addEventListener('input', onTxtNuevoArticuloInput)
 btnAñadirArticulo.addEventListener('click', onBtnAñadirArticuloClick)
@@ -11,7 +12,26 @@ function onTxtNuevoArticuloInput(evento){
 }//End onTxtNuevoArticuloInput
 
 function onBtnAñadirArticuloClick(evento){
-
+    let valorNuevoArticulo = txtNuevoArticulo.value
+    if (valorNuevoArticulo.length > 0) {
+        if (!articulosEnLista.includes(valorNuevoArticulo)){
+            articulosEnLista.push(valorNuevoArticulo)
+            localStorage.setItem('listaCompra', JSON.stringify(articulosEnLista))
+            let liNuevoArticulo = document.createElement('li')
+            liNuevoArticulo.textContent = valorNuevoArticulo
+            ulLista.appendChild(liNuevoArticulo)
+        }//End if
+        if (articulosEnLista.length = 1){
+            
+            let liEliminar = document.getElementById('liNoArticulos')
+            console.log(liEliminar)
+            if(liEliminar) liEliminar.remove()
+        }//End if
+        txtNuevoArticulo.value = ""
+        btnAñadirArticulo.disabled = true
+    }//End if
+    
 }//End onBtnAñadirArticuloClick
 
+txtNuevoArticulo.value = ""
 btnAñadirArticulo.disabled = true
